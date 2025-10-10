f299e78929360efbd396f4d51122f8c62863df14
Valentine Briese <valentinegb@icloud.com>
2025-10-09 22:48:02 -0700

    Set up logging and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use poise_error::anyhow;
+use tracing::{error, info};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+
+    if let Err(err) = try_main().await {
+        error!("Fatal error: {err:#}");
+    }
+}
+
+async fn try_main() -> anyhow::Result<()> {
+    info!("Starting up");
+
+    // TODO
+
+    info!("Shutting down");
+
+    Ok(())
 }
