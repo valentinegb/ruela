4222f7c6f6dd880f874ecd69b7c6bdda8e8da25d
Valentine Briese <valentinegb@icloud.com>
2025-11-26 18:57:09 -0800

    Add auto banning

--- src/strikes.rs
+++ src/strikes.rs
@@ -1,10 +1,11 @@
 use std::{
     ops::{Deref, DerefMut},
     str::FromStr,
+    time::UNIX_EPOCH,
 };
 
 use crate::{
-    data::{Attribution, Data, INVOCABLE_IN_GUILD},
+    data::{Attribution, Data, INVOCABLE_IN_GUILD, UNIX_EPOCH_ELAPSED_ERR},
     rule::{Rules, rule_autocomplete},
     util::{ConfirmationPrompt, send_safety_alert},
 };
@@ -12,7 +13,7 @@ use poise::{
     CreateReply, command,
     serenity_prelude::{
         self as serenity, CreateAllowedMentions, CreateComponent, CreateContainer, CreateMessage,
-        CreateSeparator, CreateTextDisplay, Member, Mentionable, MessageFlags,
+        CreateSeparator, CreateTextDisplay, GuildId, Member, Mentionable, MessageFlags,
         colours::css::{DANGER, POSITIVE},
     },
 };
@@ -21,6 +22,7 @@ use poise_error::{
     anyhow::{self, bail},
 };
 use serde::{Deserialize, Serialize};
+use tracing::error;
 
 #[derive(Deserialize, Serialize, Default)]
 struct Strikes(Vec<Strike>);
@@ -52,10 +54,32 @@ struct Strike {
     repeal: Option<Attribution>,
 }
 
+#[derive(Deserialize, Serialize, Default)]
+struct BanThreshold(Option<usize>);
+
+impl Data<GuildId> for BanThreshold {
+    const PATH: &str = "ban_threshold.cbor";
+    const DESCRIPTOR: &str = "ban threshold";
+}
+
+impl Deref for BanThreshold {
+    type Target = Option<usize>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for BanThreshold {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
 /// Commands related to strikes.
 #[command(
     slash_command,
-    subcommands("issue", "info", "repeal", "record"),
+    subcommands("issue", "info", "repeal", "record", "ban_threshold"),
     interaction_context = "Guild"
 )]
 pub async fn strike(_ctx: poise_error::Context<'_>) -> anyhow::Result<()> {
@@ -103,16 +127,60 @@ async fn issue(
             .flags(MessageFlags::IS_COMPONENTS_V2)
             .components(components)
             .allowed_mentions(allowed_mentions);
+        let dms = member.user.create_dm_channel(ctx).await?.id.widen();
 
-        member
-            .user
-            .create_dm_channel(ctx)
-            .await?
-            .id
-            .widen()
-            .send_message(ctx.http(), message.clone())
-            .await?;
+        dms.send_message(ctx.http(), message.clone()).await?;
         send_safety_alert(ctx, message).await?;
+
+        if let Some(ban_threshold) =
+            *BanThreshold::get_data_from(ctx.guild_id().expect(INVOCABLE_IN_GUILD))?
+        {
+            let non_repealed_strikes = strikes
+                .iter()
+                .filter(|strike| strike.repeal.is_none())
+                .count();
+
+            if non_repealed_strikes >= ban_threshold {
+                if let Err(err) = dms.send_message(
+                    ctx.http(),
+                    CreateMessage::new()
+                        .flags(MessageFlags::IS_COMPONENTS_V2)
+                        .components(&[CreateComponent::Container(
+                            CreateContainer::new(&[CreateComponent::TextDisplay(
+                                CreateTextDisplay::new(format!(
+                                    "### Strike Ban Threshold Met\n\
+                                    You have accumulated **{non_repealed_strikes} strikes** and thus are being automatically banned.",
+                                )),
+                            )])
+                            .accent_color(DANGER),
+                        )]),
+                )
+                .await {
+                    error!("Failed to inform member about being banned: {err:#}");
+                }
+
+                member
+                    .ban(ctx.http(), 0, Some("Strike ban threshold met."))
+                    .await?;
+                send_safety_alert(
+                    ctx,
+                    CreateMessage::new()
+                        .flags(MessageFlags::IS_COMPONENTS_V2)
+                        .components(&[CreateComponent::Container(
+                            CreateContainer::new(&[CreateComponent::TextDisplay(
+                                CreateTextDisplay::new(format!(
+                                    "### Strike Ban Threshold Met\n\
+                                    {} accumulated **{non_repealed_strikes} strikes** and thus is being automatically banned.",
+                                    member.mention(),
+                                )),
+                            )])
+                            .accent_color(DANGER),
+                        )])
+                        .allowed_mentions(CreateAllowedMentions::new()),
+                )
+                .await?;
+            }
+        }
     }
 
     Ok(())
@@ -282,6 +350,115 @@ async fn record(
             .components(&[CreateComponent::Container(
                 CreateContainer::new(container_components).accent_color(color),
             )])
+            .allowed_mentions(CreateAllowedMentions::new()),
+    )
+    .await?;
+
+    Ok(())
+}
+
+/// If a person's number of strikes meet the the threshold, they will be auto-banned.
+#[command(slash_command, subcommands("get", "set"))]
+async fn ban_threshold(_ctx: poise_error::Context<'_>) -> anyhow::Result<()> {
+    unreachable!("/strike ban_threshold has subcommands and itself should not be invocable")
+}
+
+/// Shows the current strike ban threshold.
+#[command(slash_command, required_permissions = "MANAGE_GUILD", ephemeral)]
+async fn get(ctx: poise_error::Context<'_>) -> anyhow::Result<()> {
+    let ban_threshold = BanThreshold::get_data_from(ctx.guild_id().expect(INVOCABLE_IN_GUILD))?;
+    let description = match *ban_threshold {
+        Some(ban_threshold) => &format!(
+            "Currently, members are automatically banned if they accumulate **{ban_threshold} strikes**.",
+        ),
+        None => "The strike ban threshold is not set so members are not automatically banned.",
+    };
+    let strike_ban_threshold_set_command_mention = if let Some(command) = ctx
+        .http()
+        .get_global_commands()
+        .await?
+        .into_iter()
+        .find(|command| command.name == "strike")
+    {
+        &format!("</strike ban_threshold set:{}>", command.id)
+    } else {
+        "/strike ban_threshold set"
+    };
+
+    ctx.send(
+        CreateReply::new()
+            .flags(MessageFlags::IS_COMPONENTS_V2)
+            .components(&[CreateComponent::Container(CreateContainer::new(&[
+                CreateComponent::TextDisplay(CreateTextDisplay::new(format!(
+                    "### Strike Ban Threshold\n{description}"
+                ))),
+                CreateComponent::Separator(CreateSeparator::new(true)),
+                CreateComponent::TextDisplay(CreateTextDisplay::new(format!(
+                    "-# You can change this with {strike_ban_threshold_set_command_mention}."
+                ))),
+            ]))]),
+    )
+    .await?;
+
+    Ok(())
+}
+
+/// Sets the stike ban threshold.
+#[command(slash_command, required_permissions = "MANAGE_GUILD", ephemeral)]
+async fn set(
+    ctx: poise_error::Context<'_>,
+    #[description = "The number of strikes to set the threshold to. If omitted, members will never be auto-banned."]
+    strikes: Option<usize>,
+) -> anyhow::Result<()> {
+    let guild_id = ctx.guild_id().expect(INVOCABLE_IN_GUILD);
+    let current_ban_threshold = BanThreshold::get_data_from(guild_id)?;
+    let new_ban_threshold = BanThreshold(strikes);
+
+    if *new_ban_threshold == *current_ban_threshold {
+        bail!(
+            UserError::from_str("The current strike ban threshold is already set to this.")
+                .unwrap()
+        );
+    }
+
+    new_ban_threshold.set_data_for(guild_id)?;
+
+    let description = match *new_ban_threshold {
+        Some(ban_threshold) => &format!(
+            "Now, members will be automatically banned if they accumulate **{ban_threshold} strikes**.",
+        ),
+        None => {
+            "The strike ban threshold is no longer set so members will not be automatically banned."
+        }
+    };
+    let main_text = CreateComponent::TextDisplay(CreateTextDisplay::new(format!(
+        "### Strike Ban Threshold\n{description}"
+    )));
+
+    ctx.send(
+        CreateReply::new()
+            .flags(MessageFlags::IS_COMPONENTS_V2)
+            .components(vec![CreateComponent::Container(CreateContainer::new(
+                vec![main_text.clone()],
+            ))]),
+    )
+    .await?;
+    send_safety_alert(
+        ctx,
+        CreateMessage::new()
+            .flags(MessageFlags::IS_COMPONENTS_V2)
+            .components(&[CreateComponent::Container(CreateContainer::new(&[
+                main_text,
+                CreateComponent::Separator(CreateSeparator::new(true)),
+                CreateComponent::TextDisplay(CreateTextDisplay::new(format!(
+                    "-# Set by {} on <t:{}>.",
+                    ctx.author().mention(),
+                    UNIX_EPOCH
+                        .elapsed()
+                        .expect(UNIX_EPOCH_ELAPSED_ERR)
+                        .as_secs(),
+                ))),
+            ]))])
             .allowed_mentions(CreateAllowedMentions::new()),
     )
     .await?;
